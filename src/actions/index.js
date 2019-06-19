c04ea0ae350d1cee6ca00483a79818d7d5b777c4
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-19 13:02:03 +0100

    Add action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,40 @@
+import axios from 'axios';
 // we'll need axios
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-export const types = {};
+export const actionTypes = {};
 
-types.SET_CHARACTERS = 'SET_CHARACTERS';
-types.FETCH_CHARACTERS = 'FETCH_CHARACTERS';
-types.FETCH_FAILED = 'FETCH_FAILED';
+actionTypes.SET_CHARACTERS = 'SET_CHARACTERS';
+actionTypes.FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+actionTypes.FETCH_FAILED = 'FETCH_FAILED';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+export const apiBaseURL = 'https://swapi.co/api/people/';
+export function setCharacters(characters) {
+    return {
+        type: actionTypes.SET_CHARACTERS,
+        payload: characters,
+    };
+}
+
+
+// TODO: Retry fetch after delay
+export function fetchFailed(error) {
+    return {
+        type: actionTypes.FETCH_FAILED,
+        payload: error,
+    };
+}
+
+export const fetchCharacter = () => dispatch => {
+    axios
+        .get(apiBaseURL)
+        .then(res => {
+            dispatch(setCharacters(res.data));
+        })
+        .catch(error => {
+            dispatch(fetchFailed(error.message));
+        })
+}

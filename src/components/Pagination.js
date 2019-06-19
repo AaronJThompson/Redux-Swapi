f888026b84d3f19caa2cf673ddc1f3ad2484929b
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-19 15:12:01 +0100

    Stretch: Connect pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,28 +1,70 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { fetchCharacters } from '../actions'
+import styled from 'styled-components';
+
+const ButtonsContainer = styled.div`
+    margin: 2rem 0;
+
+    button {
+        font-size: 2rem;
+        font-weight: 500;
+        padding: 0.5rem 1rem;
+        border: none;
+        background-color: #262122;
+        color: #F2F2F2;
+        border-radius: .5rem;
+    }
+
+    .previous-page{
+        margin-right: 1rem;
+    }
+`
 
 export default function Pagination(props) {
-    let { nextPage, previousPage, nextPageCB, previousPageCB } = props;
     let nextStyle = {
-        visibility: nextPage ? "initial" : "hidden",
+        visibility: props.nextPage ? "initial" : "hidden",
     };
     let previousStyle = {
-        visibility: previousPage ? "initial" : "hidden",
+        visibility: props.previousPage ? "initial" : "hidden",
     };
+
+    let goNext = () => {
+        props.fetchCharacters(props.nextPage);
+    }
+    let goPrevious = () => {
+        props.fetchCharacters(props.previousPage);
+    }
+
     return (
-        <div className="pagination">
+        <div>
             <button
                 style={previousStyle}
-                onClick={previousPageCB}
+                onClick={goPrevious}
+                className='previous-page'
             >
                 {"<"}
             </button>
 
             <button
                 style={nextStyle}
-                onClick={nextPageCB}
+                onClick={goNext}
             >
                 {">"}
             </button>
         </div>
     );
 }
+
+function mapStateToProps(state) {
+return {
+    nextPage: state.chars.nextPage,
+    previousPage: state.chars.previousPage
+};
+}
+export default connect(
+mapStateToProps,
+{
+    fetchCharacters
+}
+)(Pagination);

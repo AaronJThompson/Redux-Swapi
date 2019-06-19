7dac29307f9a264fbdd5dc36cac8a91978fbd8aa
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-19 14:08:09 +0100

    Strech: Refactor to styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,47 @@
 import React from "react";
+import styled from 'styled-components';
 
+const InfoContainer = styled.div`
+  width: 100%;
+  background: linear-gradient(180deg, #FF0000 0%, #930000 100%);
+  box-shadow: 4px 0px 4px rgba(0, 0, 0, 0.25);
+  clip-path: polygon(0 0, 66% 0, 90% 100%, 0% 100%);
+
+  h2{
+      font-weight: 500;
+      margin-bottom: 1rem;
+  }
+`;
+
+const StyledCharacter = styled.div`
+    min-width: 20rem;
+    background-color: #ff8a3d;
+    border-radius: .8rem;
+    font-size: 1.2rem;
+    margin: 1rem 0.5rem;
+    padding: 1rem;
+    color: white;
+`;
+
+const InfoList = styled.ul`
+  li{
+      text-align: left;
+  }
+  li span{
+      font-family: 'Roboto Condensed', sans-serif;
+      font-weight: 200;
+  }
+`
 const Character = props => {
-  let { name, birthYear, gender, hairColor, height } = props.character;
+  let { name, birth_year, gender, hair_color, height } = props.character;
 
   return (
-      <div className="character">
+      <StyledCharacter>
+        <InfoContainer>
           <h2>{name}</h2>
-          <ul>
+          <InfoList>
               <li>
-                  Birth Year: <span>{birthYear}</span>
+                  Birth Year: <span>{birth_year}</span>
               </li>
 
               <li>
@@ -16,14 +49,15 @@ const Character = props => {
               </li>
 
               <li>
-                  Hair Color: <span>{hairColor}</span>
+                  Hair Color: <span>{hair_color}</span>
               </li>
               
               <li>
                   Height: <span>{height}</span>
               </li>
-          </ul>
-      </div>
+          </InfoList>
+        </InfoContainer>
+      </StyledCharacter>
   )
 };
 

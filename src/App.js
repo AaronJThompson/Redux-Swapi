14d02bddbf96428be2ff3867724b879bf64b4695
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-19 13:12:56 +0100

    Create store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { CharacterListView } from "./views";
+import combinedReducers from './reducers';
+
 import "./styles/App.css";
+
+const store = createStore(
+  combinedReducers,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
 
 class App extends Component {
   render() {
-    return <CharacterListView />;
+    return (
+      <Provider store={store}>
+        <CharacterListView />
+      </Provider>
+    );
   }
 }
 

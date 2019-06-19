358c439504258eacce1426b75c7be08637793877
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-19 13:42:41 +0100

    Strech: Start styling. Pull all data into component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Character = props => {
-  return <li>{props.character.name}</li>;
+  let { name, birthYear, gender, hairColor, height } = props.character;
+
+  return (
+      <div className="character">
+          <h2>{name}</h2>
+          <ul>
+              <li>
+                  Birth Year: <span>{birthYear}</span>
+              </li>
+
+              <li>
+                  Gender: <span>{gender}</span>
+              </li>
+
+              <li>
+                  Hair Color: <span>{hairColor}</span>
+              </li>
+              
+              <li>
+                  Height: <span>{height}</span>
+              </li>
+          </ul>
+      </div>
+  )
 };
 
 export default Character;
